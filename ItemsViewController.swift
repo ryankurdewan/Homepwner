bbcd8effa987bd6f48d9f3650d355c7aa540ae61
ryankurdewan <rjk117@hotmail.com>
2017-06-21 18:36:23 -0400

    moved the tableView down so it doesn't overlap with the status bar

--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -14,10 +14,18 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         let item = itemStore.allItems[indexPath.row]
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Get the height of the status bar 
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
     }
 }
